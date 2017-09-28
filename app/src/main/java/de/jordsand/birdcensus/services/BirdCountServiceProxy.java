10387d541609125cbe95960c0d12ce6bee551cb3
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2017-09-28 16:21:37 +0200

    Adding the service implementation. See cbd34081517f723917afb37e1761a68aaa695151
    If a new bird count is started through an activity, the BirdCount object would have to be passed to every sub-activity. Thus the BirdCount class and each subclass would have to implement the Parcelable interface for reasonable performance. By using a separate service we circumvent this and provide an elegant solution for accessing the current bird count as well as adding observations.
    
    To create a service we could either
    
    * a bound service, or
    * a started service
    
    What we want is to connect to the service through each activity. It could then modify the current bird count as it needs to. Thus using a bound service seems OK. However we face some implementation problems, as a bound service will automatically destroyed when no more activities are connected to it and each activity should disconnect not later than at Activity#onDestroy(). Thus the last connections may already be closed before the newly started activity was able to connect and the service might be closed To prevent this from happening, we use a started service: the first activity which may need the service will start it if necessary and the service will automatically stop itself when the census is finished. All activities will connect to the service the usual way.

---
+++ app/src/main/java/de/jordsand/birdcensus/services/BirdCountServiceProxy.java
@@ -0,0 +1,195 @@
+package de.jordsand.birdcensus.services;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import de.jordsand.birdcensus.core.BirdCount;
+import de.jordsand.birdcensus.core.MonitoringAreaRepository;
+import de.jordsand.birdcensus.core.Species;
+import de.jordsand.birdcensus.core.SpeciesRepository;
+import de.jordsand.birdcensus.core.WeatherData;
+
+/**
+ * A {@link BirdCountService} that does not execute its operations directly, but rather buffers
+ * them for later execution on a real service.
+ * <p>
+ * When connecting to an instance of {@link SimpleBirdCountService} the binding may take place later on.
+ * If the functionality of the service is essential for an activity, it may use this proxy until the
+ * real service is bound and then call {@link #flush(BirdCountService)} to apply all pending operations.
+ * </p>
+ */
+public class BirdCountServiceProxy implements BirdCountService {
+
+    /**
+     * This is the queue for all operations that ought to be applied.
+     * For each invoked method we have a corresponding class and will add an instance of it to the
+     * queue.
+     */
+    private Queue<BirdCountOperation> cachedOperations;
+
+    /**
+     * Just the constructor
+     */
+    public BirdCountServiceProxy() {
+        this.cachedOperations = new LinkedList<>();
+    }
+
+    /**
+     * Applies all pending operations
+     * @param realService the real service to use
+     */
+    public void flush(BirdCountService realService) {
+        while(!cachedOperations.isEmpty()) {
+            cachedOperations.poll().apply(realService);
+        }
+    }
+
+    @Nullable @Override
+    public BirdCount getCurrentBirdCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    @NonNull @Override
+    public MonitoringAreaRepository getAreaRepository() {
+        throw new UnsupportedOperationException();
+    }
+
+    @NonNull @Override
+    public SpeciesRepository getSpeciesRepository() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void startBirdCount(@NonNull Date startDate, @NonNull String observerName, @NonNull WeatherData weatherData) {
+        cachedOperations.add(new StartBirdCountOperation(startDate, observerName, weatherData));
+    }
+
+    @Override
+    public void addSightingToCurrentBirdCount(@NonNull String areaCode, @NonNull Species species, int count) {
+        cachedOperations.add(new AddSightingOperation(areaCode, species, count));
+    }
+
+    @Nullable @Override
+    public Species addNewSpecies(@NonNull String name, @Nullable String scientificName) {
+        cachedOperations.add(new AddNewSpeciesOperation(name, scientificName));
+        return new Species(name, scientificName);
+    }
+
+    @Override
+    public void terminateBirdCount() {
+        cachedOperations.add(new TerminateBirdCountOperation());
+    }
+
+    @Override
+    public void abortBirdCount() {
+        cachedOperations.add(new AbortBirdCountOperation());
+    }
+
+    @Override
+    public boolean isBirdCountOngoing() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Base class for all operations
+     */
+    private abstract class BirdCountOperation {
+
+        /**
+         * Executes the operation on a (real) service instance
+         * @param birdCountService the service to use
+         */
+        abstract void apply(BirdCountService birdCountService);
+    }
+
+    /**
+     * Wrapper for the {@link BirdCountService#startBirdCount(Date, String, WeatherData)} method
+     */
+    private class StartBirdCountOperation extends BirdCountOperation {
+        private Date startDate;
+        private String observerName;
+        private WeatherData weatherData;
+
+        /**
+         * We need start date, observer name and weather data
+         */
+        StartBirdCountOperation(Date startDate, String observerName, WeatherData weatherData) {
+            this.startDate = startDate;
+            this.observerName = observerName;
+            this.weatherData = weatherData;
+        }
+
+        @Override
+        void apply(BirdCountService birdCountService) {
+            birdCountService.startBirdCount(startDate, observerName, weatherData);
+        }
+    }
+
+    /**
+     * Wrapper for the {@link BirdCountService#addSightingToCurrentBirdCount(String, Species, int)} method
+     */
+    private class AddSightingOperation extends BirdCountOperation {
+        private String areaCode;
+        private Species species;
+        private int count;
+
+        /**
+         * We need area, species and count
+         */
+        AddSightingOperation(String areaCode, Species species, int count) {
+            this.areaCode = areaCode;
+            this.species = species;
+            this.count = count;
+        }
+
+        @Override
+        void apply(BirdCountService birdCountService) {
+            birdCountService.addSightingToCurrentBirdCount(areaCode, species, count);
+        }
+    }
+
+    /**
+     * Wrapper for the {@link BirdCountService#addNewSpecies(String, String)} method
+     */
+    private class AddNewSpeciesOperation extends BirdCountOperation {
+        private String name;
+        private String scientificName;
+
+        /**
+         * We need name and scientific name
+         */
+        AddNewSpeciesOperation(String name, String scientificName) {
+            this.name = name;
+            this.scientificName = scientificName;
+        }
+
+        @Override
+        void apply(BirdCountService birdCountService) {
+            birdCountService.addNewSpecies(name, scientificName);
+        }
+    }
+
+    /**
+     * Wrapper for the {@link BirdCountService#terminateBirdCount()} method
+     */
+    private class TerminateBirdCountOperation extends  BirdCountOperation {
+        @Override
+        void apply(BirdCountService birdCountService) {
+            birdCountService.terminateBirdCount();
+        }
+    }
+
+    /**
+     * Wrapper for the {@link BirdCountService#abortBirdCount()} method
+     */
+    private class AbortBirdCountOperation extends BirdCountOperation {
+        @Override
+        void apply(BirdCountService birdCountService) {
+            birdCountService.abortBirdCount();
+        }
+    }
+}
---
+++ app/src/main/java/de/jordsand/birdcensus/services/SimpleBirdCountService.java
@@ -0,0 +1,234 @@
+package de.jordsand.birdcensus.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.util.LongSparseArray;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import de.jordsand.birdcensus.core.BirdCount;
+import de.jordsand.birdcensus.core.BirdCountRepository;
+import de.jordsand.birdcensus.core.ExistingSpeciesException;
+import de.jordsand.birdcensus.core.MonitoringArea;
+import de.jordsand.birdcensus.core.MonitoringAreaRepository;
+import de.jordsand.birdcensus.core.Species;
+import de.jordsand.birdcensus.core.SpeciesRepository;
+import de.jordsand.birdcensus.core.WeatherData;
+import de.jordsand.birdcensus.database.BirdCountOpenHandler;
+import de.jordsand.birdcensus.database.repositories.SQLiteBirdCountRepository;
+import de.jordsand.birdcensus.database.repositories.SQLiteMonitoringAreaRepository;
+import de.jordsand.birdcensus.database.repositories.SQLiteSpeciesRepository;
+
+/**
+ * Implementation of the {@link BirdCountService} to connect through different activities.
+ * <p>
+ *     <h2>Overview</h2>
+ * If a new bird count is started through an activity, the {@link BirdCount} object would have to be
+ * passed to every sub-activity. Thus the {@link BirdCount} class and each subclass would have to
+ * implement the {@link android.os.Parcelable} interface for reasonable performance. By using a
+ * separate service we circumvent this and provide an elegant solution for accessing the current
+ * bird count as well as adding observations.
+ * </p>
+ * <p>
+ *     <h2>Technical details</h2>
+ * See: <a href="https://developer.android.com/guide/components/services.html">https://developer.android.com/guide/components/services.html</a>
+ * <br>
+ * To create a service we could either
+ * <ul>
+ *     <li>a bound service, or</li>
+ *     <li>a started service</li>
+ * </ul>
+ * What we want is to connect to the service through each activity. It could then modify the current
+ * bird count as it needs to. Thus using a bound service seems OK. However we face some implementation
+ * problems, as a bound service will automatically destroyed when no more activities are connected to it
+ * and each activity should disconnect not later than at {@link android.app.Activity#onDestroy()}.
+ * Thus the last connections may already be closed before the newly started activity was able to connect
+ * and the service might be closed.
+ * To prevent this from happening, we use a started service: the first activity which may need the service
+ * will start it if necessary and the service will automatically stop itself when the census is finished.
+ * All activities will connect to the service the usual way.
+ * </p>
+ */
+public class SimpleBirdCountService extends Service implements BirdCountService {
+    private static final String TAG = SimpleBirdCountService.class.getSimpleName();
+
+    /**
+     * Flag to indicate whether the service is started
+     */
+    private static boolean running = false;
+
+    /**
+     * The binder used to connect to the service instance
+     */
+    private final IBinder mBinder = new BirdCountBinder();
+
+    private BirdCountRepository birdCountRepository;
+    private SpeciesRepository speciesRepository;
+    private MonitoringAreaRepository areaRepository;
+
+    private BirdCount currentBirdCount = null;
+
+    private LongSparseArray<Species> speciesCache;
+    private Map<String, MonitoringArea> areaCache;
+
+    /**
+     * @return {@code true} if the service is running, {@code false} otherwise
+     */
+    public static boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public void onCreate() {
+        BirdCountOpenHandler openHandler = BirdCountOpenHandler.instance(this);
+        birdCountRepository = new SQLiteBirdCountRepository(openHandler.getWritableDatabase());
+        speciesRepository = new SQLiteSpeciesRepository(openHandler.getWritableDatabase());
+        areaRepository = new SQLiteMonitoringAreaRepository(openHandler.getReadableDatabase());
+
+        speciesCache = new LongSparseArray<>();
+        areaCache = new HashMap<>();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        running = true;
+
+        return START_REDELIVER_INTENT;
+    }
+
+    @Nullable @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Nullable @Override
+    public BirdCount getCurrentBirdCount() {
+        return currentBirdCount;
+    }
+
+    @NonNull @Override
+    public MonitoringAreaRepository getAreaRepository() {
+        return areaRepository;
+    }
+
+    @NonNull @Override
+    public SpeciesRepository getSpeciesRepository() {
+        return speciesRepository;
+    }
+
+    @Override
+    public void startBirdCount(@NonNull Date startDate, @NonNull String observerName, @NonNull WeatherData weatherData) {
+        if (currentBirdCount != null) {
+            throw new IllegalStateException("Another bird count has already started");
+        }
+        currentBirdCount = new BirdCount(startDate, observerName, weatherData);
+    }
+
+    @Override
+    public void addSightingToCurrentBirdCount(@NonNull String areaCode, @NonNull Species species, int count) {
+        if (currentBirdCount == null) {
+            throw new IllegalStateException("No bird count ongoing");
+        } else if (count == 0) {
+            return;
+        }
+        MonitoringArea area = retrieveMonitoringArea(areaCode);
+        currentBirdCount.addToWatchlist(area, species, count);
+    }
+
+    @Nullable @Override
+    public Species addNewSpecies(@NonNull String name, @Nullable String scientificName) {
+        if (scientificName != null && !scientificName.isEmpty() && speciesRepository.findByScientificName(scientificName) != null) {
+            throw new ExistingSpeciesException(String.format("There already is such a species (%s, %s)", name, scientificName));
+        } else if (scientificName != null && scientificName.isEmpty() && !speciesRepository.findByName(name).isEmpty()) {
+            throw new ExistingSpeciesException(String.format("There already is such a species (%s, %s)", name, scientificName));
+        }
+
+        Species species = new Species(name, scientificName);
+        long id = speciesRepository.save(species);
+        if (id < 0) {
+            return null;
+        }
+        speciesCache.append(id, species);
+        return species;
+    }
+
+    @Override
+    public void terminateBirdCount() {
+        if (currentBirdCount == null) {
+            throw new IllegalStateException("No ongoing bird count");
+        }
+        currentBirdCount.terminate();
+        birdCountRepository.save(currentBirdCount);
+
+        currentBirdCount = null;
+        running = false;
+        stopSelf();
+    }
+
+    @Override
+    public void abortBirdCount() {
+        currentBirdCount = null;
+        running = false;
+        stopSelf();
+    }
+
+    @Override
+    public boolean isBirdCountOngoing() {
+        return currentBirdCount != null;
+    }
+
+    /**
+     * Searches for a species by its id
+     * @param id the species' id
+     * @return the corresponding species
+     */
+    private Species retrieveSpecies(long id) {
+        Species species = speciesCache.get(id);
+        if (species == null) {
+            species = speciesRepository.findOne(id);
+
+        }
+        if (species != null) {
+            speciesCache.append(id, species);
+        }
+        return species;
+    }
+
+    /**
+     * Searches for a monitoring area by its code
+     * @param code the monitoring area's code
+     * @return the matching monitoring area
+     */
+    private MonitoringArea retrieveMonitoringArea(String code) {
+        MonitoringArea area = areaCache.get(code);
+        if (area == null) {
+            area = areaRepository.findOne(code);
+        }
+        if (area != null) {
+            areaCache.put(code, area);
+        }
+        return area;
+    }
+
+    /**
+     * Our binder
+     */
+    public class BirdCountBinder extends Binder {
+
+        /**
+         * Provides access to the service instance
+         * @return the service
+         */
+        public SimpleBirdCountService getService() {
+            return SimpleBirdCountService.this;
+        }
+
+    }
+}
